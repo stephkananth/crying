d6b6e160ff8182a832c5b3bba061f9e93e26f32d
Stephanie Ananth <steph.ananth@gmail.com>
2024-11-23 15:35:12 -0500

    fix indentation in ContentView

--- Crying/ContentView.swift
+++ Crying/ContentView.swift
@@ -24,15 +24,15 @@ struct ContentView: View {
                 }
                 .onDelete(perform: deleteItems)
             }
-#if os(macOS)
+            #if os(macOS)
             .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
+            #endif
             .toolbar {
-#if os(iOS)
+                #if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-#endif
+                #endif
                 ToolbarItem {
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
